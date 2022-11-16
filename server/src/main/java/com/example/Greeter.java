80e8f23cb38b58ee22f066ebf7dae1507c31960f
unknown <frunche1@gmail.com>
2022-11-15 19:20:17 -0600

    testing scan1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
-
+    String DB_PASSWORD = "0acf0bc3cf00449bad3658f14edeee6f"
     return String.format("Hello Lyse, %s!", someone);
   }
 }
